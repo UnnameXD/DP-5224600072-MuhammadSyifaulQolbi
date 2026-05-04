568f2d8690a47dfe7a9e699cd506f640185573f0
Muhammad Syifaul Qolbi <m.syifaulqolbi1005@gmail.com>
2026-05-04 08:34:44 +0700

    Refactor Game Manager

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -6,13 +6,25 @@
 void GameManager::runSession() {
     std::cout << "\n=== Run Started ===\n";
 
-    Hand hand = handGenerator.generateHand(); // generate once
-    handPlayer.playHand(hand);                // pass same hand to player
+    // Step 1: Generate Cards / chosen hand
+    Hand hand = handGenerator.generateHand(); 
+    
+    // Step 2: Player plays selected hand
+    handPlayer.playHand(hand);    
+    
+    // Step 3: Calculate base score
     Hand chosen = handPlayer.getChosenHand();
-    int score = scoringRule.scoreHand(chosen);  // score the same hand
-    bool win = blindRule.checkBlind(score);
-    int reward = rewardRule.earnMoney(win, score);
 
+    // Step 4: Modify Joker Observer
+
+    // Step 5: Joker modifies score
+
+    // Step 6: Check win/lose
+    int score = scoringRule.scoreHand(chosen);  
+    bool win = blindRule.checkBlind(score);
+
+    // Step 7: Calculate reward
+    int reward = rewardRule.earnMoney(win, score);
     std::cout << "Money gained: " << reward << "\n";
     std::cout << "=== Run Ended ===\n\n";
 }
